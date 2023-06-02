621ac136b60953e18f5b6ee902c6abfbd18b6d2b
Efendo <efe_mustafa.oener@icloud.com>
2023-06-02 18:43:02 +0200

    added localStorage

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
+from js import window
+import ast
 
 class Application:
 
     def __init__(self):
-        self.content = [] # This is the Todo list
+        if window.localStorage.getItem('content') == "['None']":
+            self.content = []
+        else:
+            self.content =  ast.literal_eval(window.localStorage.getItem('content'))# This is the Todo list
         self.todo_out = Element('todo_out') # This is a div element which displays the Todo list
         self.text_input = Element('text_input') # This is the text field for adding todo to the list
         self.del_index = Element('del_index') # This is the number field to get the ID of the item to Delete
         self.error_log = Element('error_log') # This is the div which displays custom errors
+        self.todo_out.element.innerHTML = self.CycleTodos() # Updates the content on the page
 
     def addTodo(self): # This function adds a new item to the Todo list and displays it using the CycleTodo function
         self.error_log.element.innerHTML = ""
         self.content.append(self.text_input.value)
         self.todo_out.element.innerHTML = self.CycleTodos() # Updates the content on the page
+        window.localStorage.setItem('content', self.content)
 
     def CycleTodos(self): # This function returns a variable containing properly formatted Todo's
         self.error_log.element.innerHTML = ""
@@ -26,6 +33,7 @@ class Application:
             index = int(self.del_index.value)
             del self.content[index - 1]
             self.todo_out.element.innerHTML = self.CycleTodos() # Updates the content on the page
+            window.localStorage.setItem('content', self.content)
         except IndexError: # This displays a custom error message: Element {index} does not exist
             self.error_log.element.innerHTML = f"<pre class='py-error'> Element {index} does not exist </pre>"
         except ValueError: # This displays a custom error message: Please enter a valid number
